2257dea095181a14cfb4958b223abbcc840f9c5f
Jared Luttekek <jaydude898@gmail.com>
2017-07-06 21:39:57 -0700

    vscode settings update

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ln settings.json ~/.config/Code/User/settings.json
+ln keybindings.json ~/.config/Code/User/keybindings.json
 while read -r line || [[ -n "$line" ]]; do
     code --install-extension $line
 done < $1
