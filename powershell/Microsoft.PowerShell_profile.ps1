6473a6bedc6272aa5772b5805fb597a2464757c5
Jared Lutteke <Jared.Lutteke@coxautoinc.com>
2022-08-31 09:11:02 -0400

    Update Powershell profile for Cox Automotive, remove vscode telemetry and redundant keybindings

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,11 @@
 Import-Module posh-git
 Set-Location C:\Users\JLUTTEKE\Source
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
