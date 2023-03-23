ed063f4bfdf649d8f2bbf0fba2dc8743f509ea94
Jared Lutteke <jlutteke@gmail.com>
2023-03-22 21:38:51 -0600

    Add in pathing and PowerShell 7 profile

--- InstallWindows.ps1
+++ InstallWindows.ps1
@@ -1,31 +1,34 @@
-function Set-WindowsTerminalSettings {
+function Set-WindowsTerminalSettings($path) {
     $settingsFile = "${env:LOCALAPPDATA}\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
     Remove-Item $settingsFile -Force
-    New-Item -ItemType SymbolicLink -Path $settingsFile -Target ".\windowsterminal\settings.json"
+    New-Item -ItemType SymbolicLink -Path $settingsFile -Target "$path\windowsterminal\settings.json"
 }
 
-function Set-VSCodeSettings {
+function Set-VSCodeSettings($path) {
     $keybindingsFile = "${env:APPDATA}\Code\User\keybindings.json"
     Remove-Item $keybindingsFile -Force
-    New-Item -ItemType SymbolicLink -Path $keybindingsFile -Target ".\vscode\keybindings.json"
+    New-Item -ItemType SymbolicLink -Path $keybindingsFile -Target "$path\vscode\keybindings.json"
 
     $settingsFile = "${env:APPDATA}\Code\User\settings.json"
     Remove-Item $settingsFile -Force
-    New-Item -ItemType SymbolicLink -Path $settingsFile -Target ".\vscode\settings.json"
+    New-Item -ItemType SymbolicLink -Path $settingsFile -Target "$path\vscode\settings.json"
 }
 
-function Install-VsCodeExtensions {
-    foreach ($line in Get-Content '.\vscode\extensions.txt') {
+function Install-VsCodeExtensions($path) {
+    foreach ($line in Get-Content "$path\vscode\extensions.txt") {
         code --install-extension $line
     }
 }
 
-function Set-PowerShellSettings {
-    Remove-Item $profile
-    New-Item -ItemType SymbolicLink -Path $profile -Target ".\powershell\Microsoft.PowerShell_profile.ps1"
+function Set-PowerShellSettings($path) {
+    $powerShellProfilePath = "C:\Users\Jared\Documents\PowerShell"
+    Remove-Item -Recurse -Force $powerShellProfilePath
+    New-Item -ItemType Directory $powerShellProfilePath
+    New-Item -ItemType SymbolicLink -Path "$powerShellProfilePath\profile.ps1" -Target "$path\powershell\profile.ps1"
 }
 
-Set-WindowsTerminalSettings
-Set-VSCodeSettings
-Install-VsCodeExtensions
-Set-PowerShellSettings
+$path = (Get-Location).Path
+Set-WindowsTerminalSettings $path 
+Set-VSCodeSettings $path
+Install-VsCodeExtensions $path
+Set-PowerShellSettings $path
